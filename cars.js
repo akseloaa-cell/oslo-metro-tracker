b6aaf9579ee53931d9c0ddbbb66df740e2f38232
akseloaa-cell <akseloaa@gmail.com>
2026-05-31 01:47:55 +0200

    Create cars.js

---
+++ cars.js
@@ -0,0 +1,9 @@
+export const allCars = [];
+
+for (let set = 1; set <= 115; set++) {
+  const setStr = String(set).padStart(2, "0");
+
+  for (const prefix of ["31", "32", "33"]) {
+    allCars.push(`3${prefix}${setStr}`);
+  }
+}
