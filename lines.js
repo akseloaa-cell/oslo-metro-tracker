5d78ccd110be2c8c7be2827825be5aaaf9c6faf8
akseloaa-cell <akseloaa@gmail.com>
2026-05-30 23:15:36 +0200

    Create lines.js

---
+++ lines.js
@@ -0,0 +1,83 @@
+export const metroLines = {
+  "1": [
+    "Frognerseteren",
+    "Vettakollen",
+    "Skogen",
+    "Skådalen",
+    "Midtstuen",
+    "Besserud",
+    "Holmenkollen",
+    "Voksenkollen",
+    "Ullernåsen",
+    "Ris",
+    "Østerås"
+  ],
+
+  "2": [
+    "Østerås",
+    "Eiksmarka",
+    "Lijordet",
+    "Øraker",
+    "Haslum",
+    "Gjettum",
+    "Avløs",
+    "Kolsås"
+  ],
+
+  "3": [
+    "Mortensrud",
+    "Skullerud",
+    "Bogerd",
+    "Munkelia",
+    "Skullerud",
+    "Brattlikollen",
+    "Haugerud",
+    "Tveita",
+    "Hellerud",
+    "Skøyenåsen",
+    "Godlia",
+    "Manglerud",
+    "Brynseng",
+    "Helsfyr",
+    "Ensjø",
+    "Tøyen",
+    "Stortinget"
+  ],
+
+  "4": [
+    "Vestli",
+    "Rommen",
+    "Stovner",
+    "Vestli",
+    "Haugenstua",
+    "Kalbakken",
+    "Ammerud",
+    "Grorud",
+    "Romsås",
+    "Storo",
+    "Nydalen",
+    "Blindern",
+    "Forskningsparken",
+    "Ullevål stadion",
+    "Berg",
+    "Majorstuen",
+    "Nationaltheatret",
+    "Stortinget"
+  ],
+
+  "5": [
+    "Vestli",
+    "Stovner",
+    "Romsås",
+    "Grorud",
+    "Ammerud",
+    "Kalbakken",
+    "Haugenstua",
+    "Storo",
+    "Nydalen",
+    "Ullevål stadion",
+    "Majorstuen",
+    "Nationaltheatret",
+    "Stortinget"
+  ]
+};
